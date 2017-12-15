56b19a96d5f53cd544a1050b45faf5c7d3bd4f23
Ryan Sabo <sabora920@gmail.com>
2017-12-15 16:35:36 -0500

    added more callbacks and events testing

--- src/components/__tests__/guess-list.test.js
+++ src/components/__tests__/guess-list.test.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import config from '../testConfig'
 import GuessList from '../guess-list'
-import { shallow } from 'enzyme'
-import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+// import { expect } from 'chai'
 
 describe('<GuessList />', () => {
     it('should render without crashing', () => {
         shallow(<GuessList guesses={[]} />)
+    })
+    it('should add a guess to the guess list', () => {
+        const guessValues = [2,4,5]
+        const wrapper = mount(<GuessList guesses={guessValues} />)
+        const listItem = wrapper.find('li');
+        expect(listItem.length).toEqual(guessValues.length)
+        guessValues.forEach( (value, index) => {
+            expect(listItem.at(index).text()).toEqual(value.toString())
+        })
+        expect(listItem.at(0).text()).toEqual(guessValues[0].toString());
     })
 })
