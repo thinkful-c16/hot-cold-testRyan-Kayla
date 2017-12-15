036e25ccbd234d7d0c3fd9faf87bddf91a3652a1
Ryan Sabo <sabora920@gmail.com>
2017-12-15 12:28:50 -0500

    we have tests working for game component

--- src/components/__tests__/game.test.js
+++ src/components/__tests__/game.test.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import config from '../testConfig'
 import Game from '../Game'
+import Header from '../header'
 import { shallow } from 'enzyme'
+import { expect } from 'chai'
 
 describe('<Game />', () => {
 
     it('Renders without crashing', () => {
         shallow(<Game />);
     });
+
+    it('should have make your guess in state on load', () => {
+        const wrapper = shallow(<Game />);
+        expect(wrapper.state('feedback')).to.equal('Make your guess!')
+    })
 });
